9d1a599c6cbe50fb477e71d8980882277c4f3779
Spudsz <silverfp@gmail.com>
2015-04-27 21:39:13 +1000

    Code cleanup
    Removed console logs and tidied up some comments

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the bitcoin-price-demo
  */
 var myapp = angular.module('bitcoin-price-demo');
-myapp.controller('MainCtrl', function ($scope, $filter, $resource, $log) {
+myapp.controller('MainCtrl', function ($scope, $filter, $resource) {
 
   $scope.currencies = [{code:'',name:'Loading -'}];
   $scope.topCurrencies = [{code:'AUD'},{code:'USD'},{code:'EUR'},{code:'GBP'},{code:'JPY'},{code:'CAD'},{code:'CNY'}];
@@ -23,20 +23,15 @@ myapp.controller('MainCtrl', function ($scope, $filter, $resource, $log) {
     res.get('', function(bitpay){
       // Server Returns:
       // { data: [ { code: "BTC", symbol: "฿", precision: 6, exchangePctFee: 200, payoutEnabled: true, name: "Bitcoin", plural: "Bitcoin", alts: "btc", minimum: 0.000006, payoutFields: [ "bitcoinAddress" ] }, ...
-      $log.log('Currencies: ', bitpay.data[0].code); // debug
 
-      bitpay.data.splice(0, 1); // Remove first element, is always BTC
+      // Remove first element, is always BTC
+      bitpay.data.splice(0, 1);
 
       // Find all $scope.topCurrencies in bitpay.data and remove them
-      // Loop bitpay.data once - larger array
       for (var i = 0; i < bitpay.data.length; i++) {
-        // $log.log('Loop: ', i, bitpay.data[i].code); // debug
-
-        // Loop $scope.topCurrencies multiple times - smaller array
         for (var j = 0; j < $scope.topCurrencies.length; j++) {
           if (bitpay.data[i].code === $scope.topCurrencies[j].code) {
-            $log.log('GOTCHA!',i,$scope.topCurrencies[j].code); // debug
-            $scope.topCurrencies[j] = bitpay.data[i]; // Save full object
+            $scope.topCurrencies[j] = bitpay.data[i];
             bitpay.data.splice(i,1); // Remove
             i--;
           }  
@@ -46,12 +41,12 @@ myapp.controller('MainCtrl', function ($scope, $filter, $resource, $log) {
       // Join arrays so we get $scope.topCurencies at the top
       $scope.currencies = $scope.topCurrencies.concat(bitpay.data);
 
-      // Force the inital load order.
+      // Quick hack to force the inital load order.
       // Only call getBitcoinPrice once we have populated the 
-      // $scope.currencies array so we can get the currency symbol
+      // $scope.currencies array so we can get the currency.symbol
       $scope.getBitcoinPrice($scope.currencies[0]);
 
-      // Force the inital load order.
+      // Quick hack to force the inital load order.
       // Render the Bitcoin Graph
       $scope.getBitcoinGraph($scope.currencies[0].code);
     });
@@ -63,10 +58,10 @@ myapp.controller('MainCtrl', function ($scope, $filter, $resource, $log) {
     $scope.selectedCurrencyCode = selectedCurrency.code;
 
     if (selectedCurrency !== undefined) {
-      // Server Returns:
-      // { data: { code: "GBP", name: "Pound Sterling", rate: 150.032822 } }
       var res = $resource('https://bitpay.com/rates/:code');
       res.get({code:selectedCurrency.code}, function(bitpay){
+        // Server Returns:
+        // { data: { code: "GBP", name: "Pound Sterling", rate: 150.032822 } }
         $scope.btcPrice = $filter('currency')(bitpay.data.rate, selectedCurrency.symbol, 2);
       });
     }
@@ -126,13 +121,12 @@ myapp.controller('MainCtrl', function ($scope, $filter, $resource, $log) {
       //     updatedISO: "2015-04-26T00:03:00+00:00"
       //   }
       // }
-      $log.log('Coindesk: ',coindesk.bpi);
 
+      // Build graph friendly data
       var tmpArray = [];
       angular.forEach(coindesk.bpi, function(value,key) {
         tmpArray.push({x: Date.parse(key), y: value});
       });
-      $log.log('tmpArray: ', tmpArray);
       
       $scope.graphData = [{
         values: tmpArray,
